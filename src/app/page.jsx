fdc8484adbc926f2661425bde1a4bb24dbc1ac02
crtobias <tgonzalezarriola@gmail.com>
2024-03-21 07:44:57 -0300

    loadtask-web

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,24 @@
-export default function HomePage(){
+import { connectDB } from "@/utils/mongoose"
+import Task from "@/models/Task"
+
+async function loadTask(){
+  connectDB()
+  const tasks = await Task.find()
+  return tasks
+}
+
+
+export default async function HomePage(){
+  const tasks = await loadTask()
+
   return(
-    <div>HOME</div>
+    <div>
+      {tasks.map(task => (
+        <div key={task._id}>
+          <h3>{task.title}</h3>
+          <p>{task.description}</p>
+        </div>
+      ))}
+    </div>
   )
 }
